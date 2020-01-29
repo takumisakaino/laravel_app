82c1e66338aed47f44d86659af3532f3a6f5ea6c
sakaino <sakaino@gizumo-admin-12noMacBook-Pro.local>
2020-01-29 11:42:20 +0900

    タイトルの変更、一覧画面にIDカラムの追加

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,23 +1,25 @@
 @extends ('layouts.app')
 @section ('content')
 
-<h1 class="page-header">ToDo一覧</h1>
+<h1 class="page-header">{{ Auth::user()->name }}のToDo一覧</h1>
 <p class="text-right">
     <a class="btn btn-success" href="/todo/create">新規作成</a>
 </p>
 <table class="table">
     <thead class="thead-light">
         <tr>
-        <th>やること</th>
-        <th>作成日時</th>
-        <th>更新日時</th>
-        <th></th>
-        <th></th>
+            <th>ID</th>
+            <th>やること</th>
+            <th>作成日時</th>
+            <th>更新日時</th>
+            <th></th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
     @foreach ($todos as $todo)
         <tr>
+            <td class="align-middle">{{ $todo->id }}</td>
             <td class="align-middle">{{ $todo->title }}</td>
             <td class="align-middle">{{ $todo->created_at }}</td>
             <td class="align-middle">{{ $todo->updated_at }}</td>
